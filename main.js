5508ba935a41fd27903952b5f7458417008813e5
craftycram <github@craftycram.net>
2020-07-08 17:07:06 +0200

    debug

--- main.js
+++ main.js
@@ -144,47 +144,39 @@ function checkBass() {
 
 let lastChimesVal = 0;
 let directionChimes = 1;
-
+const bandsChimes = [{
+    band: 40,
+    value: 0
+},
+{
+    band: 53,
+    value: 0
+},
+{
+    band: 63,
+    value: 0
+},
+{
+    band: 79,
+    value: 0
+},
+{
+    band: 71,
+    value: 0
+}
+];
 function checkChimes() {
     let chimesSpectrum = chimesFft.analyze();
-    const bands = [{
-            band: 40,
-            value: 174
-        },
-        {
-            band: 53,
-            value: 178
-        },
-        {
-            band: 63,
-            value: 178
-        },
-        {
-            band: 79,
-            value: 178
-        },
-        {
-            band: 71,
-            value: 178
-        }
-    ];
-    bands.forEach(element => {
+    
+    bandsChimes.forEach(element => {
         let chimesValue = chimesSpectrum[element.band];
-        if (lastChimesVal > chimesValue) {
-            if (directionChimes > 0 && lastChimesVal > element.value) {
-                let chime = new Chimes(canvas.width / 2, 0, canvas.height / 2);
-                chimesArray.push(chime);
-            }
-
-            directionChimes = -1;
-        } else {
-            directionChimes = 1;
+        if (element.value < chimesValue) {
+            element.value = chimesValue;
         }
-
-        lastChimesVal = chimesValue;
     });
-
+    console.log(bandsChimes);
 }
+
 
 /*
 let chime = new Chimes(canvas.width / 2, 0, canvas.height / 2);
